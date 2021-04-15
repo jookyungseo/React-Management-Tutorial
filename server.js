c55b20f8d35c56195ea36af4df2019853f3a9290
jookyungseo <jookyungseo@gmail.com>
2021-04-15 14:58:37 +0900

    nodemon server.js commit

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
 
-app.get('/api/hello', (req, res) => {
+app.use('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id' : 1,
+          'image' : 'https://placeimg.com/64/64/1',
+          'name' : '주경서',
+          'birthday' : '860328',
+          'gender' : '남자',
+          'job' : '유부남'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '엄혜영',
+          'birthday' : '900821',
+          'gender' : '여자',
+          'job' : '유부녀'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '박준환',
+          'birthday' : '860327',
+          'gender' : '남자',
+          'job' : '쏠로'
+        }
+    ])
 
-    res.send({message:'Hello Express!'});
-});
+})
 
 app.listen(port, ()=> console.log(`Listening on port ${port}`));
